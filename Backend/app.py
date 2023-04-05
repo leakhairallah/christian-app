13a46d3ede8ccd10bd94337ec1d0eb69c6e34ddd
Christian Cayralat <christian-cayralat@hotmail.com>
2023-04-05 21:11:51 +0400

    changes

--- Backend/app.py
+++ Backend/app.py
@@ -5,23 +5,13 @@ app = Flask(__name__)
 @app.route('/annotate', methods=['POST'])
 def annotate():
     # Process the input text from the frontend
-    input_text = request.form.get('input_text')
+    sheet = request.form.get('sheetName')
+    spreadsheet = request.form.get('spreadsheetName')
     
     # Implement your annotation logic here
 
     # Return the annotated text as a JSON response
-    response = {'annotated_text': annotated_text}
-    return jsonify(response)
-
-@app.route('/annotate2', methods=['POST'])
-def annotate2():
-    # Process the input text from the frontend
-    input_text = request.form.get('input_text')
-    
-    # Implement your annotation logic here
-
-    # Return the annotated text as a JSON response
-    response = {'annotated_text': annotated_text}
+    response = {'sheet': sheet, 'spreadsheet': spreadsheet}
     return jsonify(response)
 
 if __name__ == '__main__':
