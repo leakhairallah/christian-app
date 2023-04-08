a38738e51001e7ef51a7ca851c0ec92cc8a9e0b9
Lea Khairallah <63969791+leakhairallah@users.noreply.github.com>
2023-04-08 15:19:41 +0300

    fiex config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from os import environ as env
-from azure.identity import ClientSecretCredential
+from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
 TENANT_ID = env.get("AZURE_TENANT_ID", "")
@@ -8,14 +8,10 @@ CLIENT_SECRET = env.get("AZURE_CLIENT_SECRET", "")
 
 KEYVAULT_NAME = env.get("key_vault_name", "")
 KEYVAULT_SECRET_1 = env.get("secret_name_1")
+
 KEYVAULT_URI = f"https://{KEYVAULT_NAME}.vault.azure.net/"
 
-_credential = ClientSecretCredential(
-    tenant_id = TENANT_ID,
-    client_id = CLIENT_ID,
-    client_secret = CLIENT_SECRET
-)
-
+_credential = DefaultAzureCredential()
 _serviceClient = SecretClient(vault_url=KEYVAULT_URI, credential=_credential)
 
 GSPREAD_API_KEY = _serviceClient.get_secret(KEYVAULT_SECRET_1).value
