34955156cb5bc946219498b44128c9af4162bd62
Christian Cayralat <christian-cayralat@hotmail.com>
2023-04-08 14:07:52 +0300

    camel_morph build, pip install

--- App/app.py
+++ App/app.py
@@ -1,14 +1,41 @@
 from flask import Flask, request, jsonify, render_template
 
+import gspread
+
+from camel_morph.sandbox.debug_lemma_paradigms import regenerate_signature_lex_rows
+from camel_morph.utils import utils
+
+CONFIG_FILE = 'config_debug_lemma_paradigms.json'
+
+config = utils.get_config_file(CONFIG_FILE)
+config_global = config['global']
+
+sa = gspread.service_account(config_global['service_account'])
+
+def get_config_name(sheet_name):
+    if 'Noun-' in sheet_name:
+        config_name = 'noun_msa_lemma_paradigms'
+    elif 'Adj-' in sheet_name:
+        config_name = 'adj_msa_lemma_paradigms'
+    else:
+        raise NotImplementedError
+    return config_name
+
 app = Flask(__name__)
 
-@app.route('/annotate', methods=['POST'])
-def annotate():
+@app.route('/regenerateSignature', methods=['POST'])
+def regenerate_signature():
     # Process the input text from the frontend
-    sheet = request.form.get('sheetName')
+    sheet_name = request.form.get('sheetName')
     spreadsheet = request.form.get('spreadsheetName')
-    
-    # Implement your annotation logic here
+
+    config_name = get_config_name(sheet_name)
+
+    sh = sa.open(spreadsheet)
+    sheets = sh.worksheets()
+
+    sheet = [sheet for sheet in sheets if sheet.title == sheet_name][0]
+    regenerate_signature_lex_rows(sheet, sh, config, config_name)
 
     # Return the annotated text as a JSON response
     response = {'sheet': sheet, 'spreadsheet': spreadsheet}
@@ -16,7 +43,10 @@ def annotate():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    sheets = [sheet for config_local in config['local'].values()
+              for sheet in config_local['debugging']['sheets']]
+    return render_template('index.html',
+                           sheets=sheets)
 
 if __name__ == '__main__':
     app.run(debug=True)
